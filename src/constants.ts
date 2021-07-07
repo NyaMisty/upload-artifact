7c8bfced419044d3666c4159922bb27ee2622092
NyaMistyBuildWorker <buildworker@misty.moe>
2021-07-07 00:54:39 +0000

    Initial commit

---
+++ src/constants.ts
@@ -0,0 +1,23 @@
+export enum Inputs {
+  Name = 'name',
+  Path = 'path',
+  IfNoFilesFound = 'if-no-files-found',
+  RetentionDays = 'retention-days'
+}
+
+export enum NoFileOptions {
+  /**
+   * Default. Output a warning but do not fail the action
+   */
+  warn = 'warn',
+
+  /**
+   * Fail the action with an error message
+   */
+  error = 'error',
+
+  /**
+   * Do not output any warnings or errors, the action does not fail
+   */
+  ignore = 'ignore'
+}
